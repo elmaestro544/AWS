2f9ec5fe6ff4d1612fb46b1d77d1a41ee5f73b03
elmaestro544 <oxygen.ml1@gmail.com>
2025-11-20 07:16:43 +0300

    Update vite.config.js
    update

--- itroadmap.casa_6kxzhu/vite.config.js
+++ itroadmap.casa_6kxzhu/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Explicitly set the base path for deployment
   server: {
     port: 3000,
     host: '0.0.0.0'
